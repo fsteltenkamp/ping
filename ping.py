a3139852b0bae25c4751c5aa200e4149e54eb211
fsteltenkamp <florian@steltenkamp.de>
2025-01-20 16:05:46 +0100

    more config for boot delay

--- ping.py
+++ ping.py
@@ -150,7 +150,7 @@ logging.getLogger().addHandler(loggingHandler)
 
 #main:
 logging.info('Starting ping script.')
-if os.environ.get('BOOT_DELAY'):
+if os.environ.get('BOOT_DELAY') and os.environ.get('BOOT_DELAY') > '0':
     logging.info('Waiting for ' + os.environ.get('BOOT_DELAY') + ' seconds to allow for other services to start.')
     time.sleep(os.environ.get('BOOT_DELAY'))
 logging.info('Starting main loop.')
